bae7760d1f897021dcb540271df8b79419d3887a
SweepOnWakeup <talentlessh@gmail.com>
2022-09-06 15:44:55 +0200

    Completed 02_repeatString and 03_reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) 
+{
+    sLine=''
+    if (num < 0)
+    {
+        return "ERROR"
+    }
+    
+    for(iCount = 0; iCount<num; iCount++)
+    {
+        sLine = sLine + string;
+    }
+    return sLine;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) 
+{
+    sWord = string; 
+    iLength = sWord.length;
+    iPos = iLength-1;
+    sRet = '';
+    for(iLoop = 0; iLoop<iLength; iLoop++)
+    {
+        sRet = sRet + sWord.charAt(iPos);
+        iPos--;
+    }
+    return sRet;
 };
 
 // Do not edit below this line
