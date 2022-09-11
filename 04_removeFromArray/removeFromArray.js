373a5969632ff0551290528f4201778c597124c2
SweepOnWakeup <talentlessh@gmail.com>
2022-09-11 21:37:46 +0200

    Completed 04_removeFromArray and 05_sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrList, ...args) 
+{
+    
+    var iCheck
+    for(iInd = 0;iInd<=args.length; iInd++)
+    {
+        
+        iCheck = arrList.indexOf(args[iInd])
+        if (iCheck>=0)
+        {
+            
+            arrList.splice(iCheck, 1)
+        }
+    }
+    return arrList
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,31 @@
-const sumAll = function() {
+const sumAll = function (iFirst, iSecond) {
+    var iLow = 0
+    var iSum = 0
+
+    //Check if parameters ar numbers
+    if(!Number.isInteger(iFirst) || !Number.isInteger(iSecond)){
+        return "ERROR"
+    }
+
+    //Check if parameters are greater or equal to zero
+    if (iFirst >= 0 && iSecond >= 0) {
+        
+        //Check which is larger
+        if (iFirst < iSecond) {
+            iLow = iFirst
+        }
+        else {
+            iLow = iSecond
+            iSecond = iFirst
+        }
+        for (iLow; iLow <= iSecond; iLow++) {
+            iSum = iSum + iLow
+        }
+        return iSum
+    }
+    else {
+        return "ERROR"
+    }
 
 };
 
