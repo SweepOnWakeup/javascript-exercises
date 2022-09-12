47d11f579703e099713664e91ce7e5a9fc451063
SweepOnWakeup <talentlessh@gmail.com>
2022-09-12 02:41:26 +0200

    Completed 06_leapYears and 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(nTemp) {  //formula f=(c*1.8)+32
+  nTemp=(nTemp-32)/1.8
+  if(nTemp%1==0){
+    return nTemp
+  }
+  fRet = parseFloat(nTemp.toFixed(1))
+  console.log(fRet)
+  return fRet
 };
 
-const ctof = function() {
-
+const ctof = function(nTemp) {  //formula c=(f-32)/1.8
+  nTemp=(nTemp*1.8)+32
+  if(nTemp%1==0){
+    return nTemp
+  }
+  fRet = parseFloat(nTemp.toFixed(1))
+  console.log(fRet)
+  return fRet
 };
 
 // Do not edit below this line
