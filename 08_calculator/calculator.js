55e4e87d89214109c71075d46d3e2169ab5fa4dd
SweepOnWakeup <talentlessh@gmail.com>
2022-12-02 05:52:17 +0200

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1-num2;
 	
 };
 
-const sum = function() {
-	
+const sum = function(arrNums) {
+  var iTot = 0
+  for(var iLoop = 0; iLoop < arrNums.length; iLoop++){
+    iTot = iTot+arrNums[iLoop]
+  }
+  return iTot
 };
 
-const multiply = function() {
-
+const multiply = function(arrNums) {
+var iTot = 1
+for(var iLoop = 0; iLoop < arrNums.length; iLoop++){
+  iTot = iTot*arrNums[iLoop]
+}
+return iTot
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  if(num1 == 0){
+    return 1
+  }
+  var iTot = 1
+	for(var iLoop = 1; iLoop <= num1; iLoop++){
+    iTot = iTot*iLoop
+  }
+  return iTot
 };
 
 // Do not edit below this line
