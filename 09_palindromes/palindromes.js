dda8134b7dfb8c412cec6cca8d069253a9d77c1f
SweepOnWakeup <talentlessh@gmail.com>
2022-12-03 06:29:29 +0200

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (sLine) {
+    sLine = sLine.replace(/[^a-zA-Z0-9]/g,'')
+    sLine = sLine.toLowerCase()
+    var sCheck = ''
+    var iLoop = sLine.length - 1
+    for(iLoop; iLoop>=0; iLoop--){
+        sCheck = sCheck + sLine.charAt(iLoop)
+    }
 
+    return sLine === sCheck
 };
 
 // Do not edit below this line
