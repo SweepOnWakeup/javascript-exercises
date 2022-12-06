d207edb926222b21c5f43006755ab986c987ec83
SweepOnWakeup <talentlessh@gmail.com>
2022-12-06 07:28:11 +0200

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(iNum) {
+    const arrNums = [1, 1]
+
+    if(iNum == 1 || iNum == 2){
+        return 1
+    }
+    if(iNum<1){
+        return "OOPS"
+    }
+
+    for(var iLoop = 2; iLoop < iNum; iLoop++){
+        arrNums[arrNums.length] = arrNums[iLoop-1] + arrNums[iLoop-2]
+    }
+    return arrNums[arrNums.length - 1]
 
 };
 
