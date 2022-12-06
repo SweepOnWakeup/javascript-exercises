27afbf177bc5697319b789c4a5e0d7955fc133e7
SweepOnWakeup <talentlessh@gmail.com>
2022-12-06 08:40:41 +0200

    Completed 11_getTheTitles and 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const currentYear = (new Date()).getFullYear();
+    var firstCheck = 0
+    var secondCheck = 0
+
+    const oldest = people.sort((a, b) =>{
+        if(a.yearOfDeath !== undefined){
+            
+            firstCheck = a.yearOfDeath - a.yearOfBirth
+        }
+        else{
+            firstCheck = currentYear - a.yearOfBirth
+        }
+
+        if(b.yearOfDeath !== undefined){
+            
+            secondCheck = b.yearOfDeath - b.yearOfBirth
+        }
+        else{
+            secondCheck = currentYear - b.yearOfBirth
+        }
+        
+        return firstCheck > secondCheck ? -1:1
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
